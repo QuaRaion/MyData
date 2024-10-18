c0b20604588b8053277d52578f958961417fd3e0
Sherzod Mahkamov <mahkamov@neotech-marine.ru>
2024-10-18 06:08:58 +0300

    Добавление HTML страниц

--- my_data/main/urls.py
+++ my_data/main/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_chart)
+    path('', views.render_home_page, name='home'),
+    path('files', views.render_files_page, name='files'),
+    path('charts', views.render_chars_page, name='charts'),
+    path('dashboards', views.render_dashboards_page, name='dashboards'),
 ]
--- my_data/main/views.py
+++ my_data/main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 
-def create_chart(request):
+def render_home_page(request):
+    return render(request, 'main/home_page.html')
+
+def render_files_page(request):
+    return render(request, 'main/files_page.html')
+
+def render_chars_page(request):
     return render(request, 'main/charts_page.html')
+
+def render_dashboards_page(request):
+    return render(request, 'main/dashboards_page.html')
+
