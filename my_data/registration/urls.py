f60d9cd2cd2537f1343e11ea34d8283d5228001e
4217 Makarov N.S <agiosnikita@gmail.com>
2024-12-10 02:27:37 +0300

    Политика и соглашение

--- my_data/registration/urls.py
+++ my_data/registration/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('log_in/', views.render_log_in_page, name='log_in'),
     path('', views.render_sign_up_page, name='sign_up'),
     path('home/', include('main.urls'), name='home'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('agreement/', views.agreement, name='agreement'),
 ]
