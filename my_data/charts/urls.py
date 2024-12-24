7e5ff53c5fbd84a27cdec4f4d8bdef1f10d2578e
Sherzod Mahkamov <mahkamov@neotech-marine.ru>
2024-12-24 18:53:20 +0300

    Замена plotly dash на plotly js

--- my_data/charts/urls.py
+++ my_data/charts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('error/', views.render_error_page, name='error'),
     # path('<int:pk>/', NewChart.as_view(), name='create_chart'),
     path('<int:pk>/', views.create_chart_page, name='create_chart'),
+    # path('save_chart/', views.save_chart, name='save_chart'),
 ]
